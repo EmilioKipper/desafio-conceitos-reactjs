1b15e0d09665953485b20c60ab30ec607ce2891c
Emílio Gularte Kipper <emilio.kipper@rede.ulbra.br>
2020-04-08 20:20:59 -0300

    Challenge Finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepos(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      url: "http://google.com",
+      title: `Title ${Date.now()}`,
+      techs: ["node", "react"]
+    })
+
+    const repository = response.data;
+
+    setRepos([...repos, repository])
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    await api.delete(`repositories/${id}`);
+
+    const newState = repos.filter(repo => repo.id !== id);
+
+    setRepos(newState);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {
+          repos && repos.map(repo => (
+            <li key={repo.id}>
+              {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+            </button>
+            </li>
+          ))
+        }
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
